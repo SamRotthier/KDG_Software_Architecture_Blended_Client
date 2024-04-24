abc7faca3e488859a9e6770263de7235e5c6dbc6
Matthias Vermeiren <matthias.vermeiren@student.kdg.be>
2024-04-24 19:30:01 +0200

    start on AccountController

--- src/main/java/be/kdg/sa/clients/controller/AccountController.java
+++ src/main/java/be/kdg/sa/clients/controller/AccountController.java
@@ -1,10 +1,23 @@
 package be.kdg.sa.clients.controller;
 
+import be.kdg.sa.clients.controller.dto.AccountDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/account") //path to be checked
 public class AccountController {
-    //TODO
+    public AccountController() {
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<AccountDto> createAccount(@RequestParam String name){
+        // TODO
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
---
+++ src/main/java/be/kdg/sa/clients/controller/dto/AccountDto.java
@@ -0,0 +1,74 @@
+package be.kdg.sa.clients.controller.dto;
+
+import be.kdg.sa.clients.domain.Enum.AccountRelationType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class AccountDto {
+    @NotNull
+    @NotEmpty
+    private String lastName;
+    @NotNull
+    @NotEmpty
+    private String firstName;
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    private String company;
+
+    @NotNull
+    @NotEmpty
+    private AccountRelationType type;
+
+    public AccountDto() {
+    }
+
+    public AccountDto(String lastName, String firstName, String email, String company, AccountRelationType type) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.company = company;
+        this.type = type;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public AccountRelationType getType() {
+        return type;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setType(AccountRelationType type) {
+        this.type = type;
+    }
+}
