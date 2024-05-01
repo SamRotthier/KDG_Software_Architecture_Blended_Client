651b79233c2b6ba39197c9d666e67ab6c6081397
Matthias Vermeiren <matthias.vermeiren@student.kdg.be>
2024-05-01 14:53:27 +0200

    Hibernate database debugging with Sam

---
+++ src/main/java/be/kdg/sa/clients/domain/Order.java
@@ -0,0 +1,111 @@
+package be.kdg.sa.clients.domain;
+
+import be.kdg.sa.clients.domain.Enum.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderId;
+    @OneToMany
+    private List<Product> products;
+    private OrderStatus status;
+    private double totalPrice;
+    private LocalDateTime creationDateTime;
+    private LocalDateTime modifiedDateTime;
+    private LocalDateTime confirmationDateTime;
+    @ManyToOne
+    private Account account;
+
+
+    public Order(UUID orderId, List<Product> products, OrderStatus status, double totalPrice, LocalDateTime creationDateTime, LocalDateTime modifiedDateTime, LocalDateTime confirmationDateTime, Account account) {
+        this.orderId = orderId;
+        this.products = products;
+        this.status = status;
+        this.totalPrice = totalPrice;
+        this.creationDateTime = creationDateTime;
+        this.modifiedDateTime = modifiedDateTime;
+        this.confirmationDateTime = confirmationDateTime;
+        this.account = account;
+    }
+    public Order() {
+
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public LocalDateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+
+    public LocalDateTime getConfirmationDateTime() {
+        return confirmationDateTime;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setCreationDateTime(LocalDateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public void setModifiedDateTime(LocalDateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+    }
+
+    public void setConfirmationDateTime(LocalDateTime confirmationDateTime) {
+        this.confirmationDateTime = confirmationDateTime;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void cancelOrder (){
+        if (status == OrderStatus.PENDING){
+            status = OrderStatus.CANCELLED;
+        }
+    }
+
+}
---
+++ src/main/java/be/kdg/sa/clients/domain/Product.java
@@ -0,0 +1,80 @@
+package be.kdg.sa.clients.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID productId;
+    private String name;
+    private Double price;
+    private String description;
+    private int quantity;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public Product(String name, Double price, String description, int quantity, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+    public Product() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
